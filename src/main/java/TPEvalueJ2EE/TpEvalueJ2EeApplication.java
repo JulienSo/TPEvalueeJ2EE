ceedb3a93c1507b3056ad4bc63cf8a8a56630723
JulienSo <soula.julien@wanadoo.fr>
2017-04-02 17:18:16 +0200

    first commit deux

---
+++ src/main/java/TPEvalueJ2EE/TpEvalueJ2EeApplication.java
@@ -0,0 +1,12 @@
+package TPEvalueJ2EE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpEvalueJ2EeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpEvalueJ2EeApplication.class, args);
+	}
+}
---
+++ src/test/java/TPEvalueJ2EE/TpEvalueJ2EeApplicationTests.java
@@ -0,0 +1,16 @@
+package TPEvalueJ2EE;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TpEvalueJ2EeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
